2ea3ce04bc584ef88eea1d82ba74eb63b3e905b2
İkra ÖZGİRGİN <69799407+ikraozgirgin@users.noreply.github.com>
2025-01-07 12:55:01 +0300

    Add files via upload

---
+++ JavaGeneric/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	 Integer[] i= {1, 2, 3};
+	 String[] s = {"Hello", "World"};
+	 
+	 PrintArray.printArray(i);
+	 PrintArray.printArray(s);
+	 
+	 
+	}
+
+}
---
+++ JavaGeneric/src/PrintArray.java
@@ -0,0 +1,9 @@
+
+public class PrintArray {
+
+	public static <T> void printArray(T[] arr) {
+		for (T t : arr) {
+			System.out.println(t);
+		}
+	}
+}
